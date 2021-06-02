f36fc5419838ab0df8c40d8436a8e44879c02d60
Jagveer-Sangha <61700208+Jagveer-Sangha@users.noreply.github.com>
2021-06-02 13:35:34 -0400

    Update z_creator.c using provided lecture examples

--- q1/z_creator.c
+++ q1/z_creator.c
@@ -1,1 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+int main(void) {
 
+  pid_t child_pid = fork();
+  // Making the Parent Process to Sleep for some time.
+  if (child_pid > 0){ 
+    printf("In Parent Process.!\n"); 
+    sleep(10);
+  }else{ 
+    printf("In Child process.!\n"); 
+    exit(0);
+  }
+  return 0;
+}
