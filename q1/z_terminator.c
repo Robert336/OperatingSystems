eab3a27389eba2a4d51872eb891c9d28005a95d0
Jagveer-Sangha <jagveersangha@gmail.com>
2021-06-02 15:56:01 -0400

    Update z_terminator.c

--- q1/z_terminator.c
+++ q1/z_terminator.c
@@ -1,1 +1,14 @@
-
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+int main(void) {
+    system("./z_creator &");
+    system("ps -l");
+    sleep(10);
+    system("kill -9 $(ps -l|grep -w Z|tr -s ''|cut -d '' -f 5)");
+    sleep(10);
+    printf("Testing\n");
+    system("ps -l");
+    return 0;
+}
