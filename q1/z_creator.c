7f3df7fb0af0b2d458cbdf9b230cb9014c83d463
Jagveer-Sangha <jagveersangha@gmail.com>
2021-06-02 15:18:01 -0400

    Update z_creator.c

--- q1/z_creator.c
+++ q1/z_creator.c
@@ -5,12 +5,17 @@
 int main(void) {
 
   pid_t child_pid = fork();
+  printf("the pid is: %d || ", getpid());
+  // Shows the details of both processes
+  // system("ps -l");
   // Making the Parent Process to Sleep for some time.
   if (child_pid > 0){ 
-    printf("In Parent Process.!\n"); 
+    printf("In the parent process: %d \n",getpid()); 
+    // Check process list to see the zombie process
+    // system("ps -l");
     sleep(10);
   }else{ 
-    printf("In Child process.!\n"); 
+    printf("In the child process: %d \n",getpid()); 
     exit(0);
   }
   return 0;
