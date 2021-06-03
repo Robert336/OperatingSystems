f4f3c0f6ce3071076b6bfa289578b4c1c9abbbde
Jagveer-Sangha <jagveersangha@gmail.com>
2021-06-03 10:52:40 -0400

    Added simplified comments

--- q1/z_creator.c
+++ q1/z_creator.c
@@ -4,18 +4,33 @@
 #include<unistd.h>
 int main() {
 
-  printf("the pid is: %d ", getpid());
-  pid_t child_pid = fork();
+  // printf("the pid is: %d ", getpid());
+
+  // By forking we create a new child process
+  // pid_t child_process = fork();
+  int child_process = fork();
+
   // Shows the details of both processes
   // system("ps -l");
-  // Making the Parent Process to Sleep for some time.
-  if (child_pid > 0){ 
-    printf("In the parent process: %d \n",getpid()); 
+
+// The id of parent process is >0
+  if (child_process > 0){ 
+    // printf("In the parent process: %d \n",getpid()); 
+    printf("\nIn the parent process: %d\n ",child_process); 
+
     // Check process list to see the zombie process
     // system("ps -l");
-    sleep(20);
-  }else{ 
-    printf("In the child process: %d \n",getpid());
+
+    // Variable to decide how long zombie process will remain in the system
+    int X = 20;
+    sleep(X);
+  
+  }else if (child_process<0){ //Failed fork processes value is -1
+    printf("Failed to fork process\n");
+
+  }else{  // The int value of child processes is 0 
+    // printf("In the child process: %d \n",getpid());
+    printf("In the child process: %d \n\n",child_process);
     exit(0);
   }
   return 0;
--- q1/z_terminator.c
+++ q1/z_terminator.c
@@ -3,13 +3,17 @@
 #include<string.h>
 #include<unistd.h>
 int main() {
+    // Call z_creator to create a zombie process
     system("./z_creator &");
+    // View to make sure zombie process is working
     system("ps -l");
     sleep(3);
+    // Kills the zombie process
     system("kill -9 $(ps -l|grep -w Z|tr -s ' '|cut -d ' ' -f 5)");
     
     sleep(10);
-    printf("\n\nNew states\n\n");
+    // Results to show the updated processes 
+    printf("\n\nNew Updated Process states:\n\n");
     system("ps -l");
     return 0;
 }
