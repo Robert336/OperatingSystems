b40a7f7fb0e003b1d34d339162afd7788b5e0222
Jagveer-Sangha <jagveersangha@gmail.com>
2021-06-04 16:47:54 -0400

    Updated header files

--- q2/process_manangment.c
+++ q2/process_manangment.c
@@ -3,14 +3,16 @@
     Date: 2021-06-02 (YYYY-MM-DD)
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 void readFile(void *shm_ptr);
 void writeOutput(char* command, char* output);
