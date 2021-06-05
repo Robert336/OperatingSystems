c8462e9177d22659b6e930d002a69def17810fd3
Jagveer-Sangha <jagveersangha@gmail.com>
2021-06-05 15:38:07 -0400

    Updated comments

--- q1/z_creator.c
+++ q1/z_creator.c
@@ -1,19 +1,33 @@
+/*  ************************************************************************************
+    Operating Systems 
+    Assignment 1 | Question 1
+    
+    Authors: Robert, Jagveer
+    Date: 2021-06-02 (YYYY-MM-DD)
+
+    GENERAL OBJECTIVE
+        
+    
+    ************************************************************************************
+*/
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 #include<unistd.h>
+
 int main() {
 
   // printf("the pid is: %d ", getpid());
+  // pid_t child_process = fork();
 
   // By forking we create a new child process
-  // pid_t child_process = fork();
   int child_process = fork();
 
   // Shows the details of both processes
   // system("ps -l");
 
-// The id of parent process is >0
+// The id of parent process is > 0
   if (child_process > 0){ 
     // printf("In the parent process: %d \n",getpid()); 
     printf("\nIn the parent process: %d\n ",child_process); 
@@ -22,8 +36,8 @@ int main() {
     // system("ps -l");
 
     // Variable to decide how long zombie process will remain in the system
-    int X = 20;
-    sleep(X);
+    int x = 20;
+    sleep(x);
   
   }else if (child_process<0){ //Failed fork processes value is -1
     printf("Failed to fork process\n");
--- q1/z_terminator.c
+++ q1/z_terminator.c
@@ -1,19 +1,39 @@
+/*  ************************************************************************************
+    Operating Systems 
+    Assignment 1 | Question 1
+    
+    Authors: Robert, Jagveer
+    Date: 2021-06-02 (YYYY-MM-DD)
+
+    GENERAL OBJECTIVE
+        
+    
+    ************************************************************************************
+*/
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 #include<unistd.h>
+
 int main() {
+
     // Call z_creator to create a zombie process
     system("./z_creator &");
-    // View to make sure zombie process is working
+
+    // View list of processes to make sure zombie process is working
     system("ps -l");
+
     sleep(3);
-    // Kills the zombie process
+    //Given System call to kills the zombie process
     system("kill -9 $(ps -l|grep -w Z|tr -s ' '|cut -d ' ' -f 5)");
     
     sleep(10);
+
     // Results to show the updated processes 
     printf("\n\nNew Updated Process states:\n\n");
+    //Shows the updated processes after killing the zombie process
     system("ps -l");
+
     return 0;
 }
