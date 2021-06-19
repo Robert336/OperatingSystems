2e90d9128a4c1de3944924fc967a69669f55abc0
Jagveer-Sangha <jagveersangha@gmail.com>
2021-06-19 14:56:51 -0400

    1

--- A2/q1/Question1.c
+++ A2/q1/Question1.c
@@ -13,27 +13,35 @@ void *custom_thread(void *arg);
 
 int main(int argc, char *argv[])
 {
+
     int i, scope;
     pthread_t tid[NUM_THREADS];
     pthread_attr_t attr;
     pthread_attr_init(&attr);
 
+    printf("This program would create threads\n");
     //int status = pthread_create(&t_id, &thread_attrib, (void *)&threadRun, thread);
     // Creates threads
     for (i = 0; i < NUM_THREADS; i++)
     {
         pthread_create(&tid[i], &attr, custom_thread, NULL);
     }
+
     // Joins threads
     for (i = 0; i < NUM_THREADS; i++)
     {
         pthread_join(tid[i], NULL);
     }
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        printf("I am the process created by compiler By default\n");
+        sleep(1);
+    }
 }
 
 void *custom_thread(void *arg)
 {
-    printf("yeet\n");
-    pthread_exit(0);
-    // printf("I am a Custom Thread Function Created by User");
+    printf("I am a Custom Thread Function Created by user.\n");
+    sleep(1);
 }
